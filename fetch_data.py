45e28ec8e59095e350f2fca3a43bbe3692d56d57
Ranjit Singh <78042062+Ranjit-Singh-786@users.noreply.github.com>
2024-07-28 20:14:11 +0530

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient 
 from pymongo.errors import PyMongoError 
 
-connection_string = "mongodb+srv://ranjitsingh:12453696@demo.86fki1p.mongodb.net/?retryWrites=true&w=majority&appName=Demo"
+connection_string = "mongodb+srv://ranjitsingh:<password>@demo.86fki1p.mongodb.net/?retryWrites=true&w=majority&appName=Demo"
 client = MongoClient(connection_string)
 
 databases = client.list_database_names()
